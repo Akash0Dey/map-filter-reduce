a19b7def665a43b0e4d4456f92551e50976bfb2b
Akash <deymrakash99@gmail.com>
2024-12-21 19:42:55 +0530

    3 filter done

--- filter.js
+++ filter.js
@@ -1,17 +1,39 @@
 // even numbers [1, 2, 3, 4, 5] => [2, 4]
-const filterEvenNumbers = function (numbers) { };
+const isEven = function (number) { return number % 2 === 0; }
+
+const filterEvenNumbers = function (numbers) {
+  return numbers.filter(isEven);
+}
 
 // words with more than 5 letters ["apple", "banana", "kiwi", "grape"] => ["banana"]
-const filterLongWords = function (words) { };
+const isStringLengthMoreThan = function (length) {
+  return function (string) {
+    return string.length > length;
+  }
+}
+
+const isStringLengthMoreThan5 = isStringLengthMoreThan(5);
+
+const filterLongWords = function (words) {
+  return words.filter(isStringLengthMoreThan5);
+}
 
 // people older than 30 [{name: "Alice", age: 25}, {name: "Bob", age: 35}] => [{name: "Bob", age: 35}]
-const filterAdults = function (people) { };
+const isOlderThan = function (age) {
+  return function (person) {
+    return person.age > age;
+  }
+}
+
+const isOlderThan30 = isOlderThan(30);
+
+const filterAdults = function (people) { return people.filter(isOlderThan30); }
 
 // active users [{username: "alice", active: true}, {username: "bob", active: false}] => [{username: "alice", active: true}]
 const filterActiveUsers = function (users) { };
 
 // numbers greater than 10 [5, 12, 7, 18, 3] => [12, 18]
-const filterNumbersGreaterThanTen = function (numbers) { };
+function filterNumbersGreaterThanTen(numbers) { }
 
 // books with more than 200 pages [{title: "Book 1", pages: 150}, {title: "Book 2", pages: 250}] => [{title: "Book 2", pages: 250}]
 const filterLongBooks = function (books) { };
@@ -215,13 +237,13 @@ const filterUsersByPostComments = function (users, minComments) { };
 const filterUsersByPostCategory = function (users, category) { };
 
 // Filter users who have a certain number of followers and have posted in the last 30 days [{user: {name: "Tom", followers: 1000, lastPostDate: "2024-11-10"}}] => [{user: {name: "Tom", followers: 1000, lastPostDate: "2024-11-10"}}]
-const filterActiveUsers = function (users, minFollowers, daysAgo) { };
+const filterActiveUser = function (users, minFollowers, daysAgo) { };
 
 // Filter posts that have at least one hashtag from a list of trending hashtags [{post: {title: "Post 1", hashtags: ["#food", "#vegan"]}}] => [{post: {title: "Post 1", hashtags: ["#food", "#vegan"]}}]
 const filterPostsByHashtags = function (posts, trendingHashtags) { };
 
 // Filter users who have shared at least one post that received a specific number of likes [{user: {name: "Lucy", posts: [{title: "Post 1", likes: 500}, {title: "Post 2", likes: 100}]}}] => [{user: {name: "Lucy", posts: [{title: "Post 1", likes: 500}, {title: "Post 2", likes: 100}]}}]
-const filterUsersByPostLikes = function (users, minLikes) { };
+const filterUsersByPostLike = function (users, minLikes) { };
 
 // Filter posts that have a certain number of comments and are from a specific location [{post: {title: "Post 1", comments: 150, location: "Paris"}}] => [{post: {title: "Post 1", comments: 150, location: "Paris"}}]
 const filterPostsByCommentsAndLocation = function (posts, minComments, location) { };
@@ -337,3 +359,28 @@ const findInStockItems = function (items, lookup) { };
 // Input: ["Lion", "Elephant", "Shark"], { "Lion": { habitat: "Jungle" }, "Elephant": { habitat: "Jungle" }, "Shark": { habitat: "Ocean" } } , "Jungle"
 // Output: ["Lion", "Elephant"]
 const findAnimalsByHabitat = function (animals, lookup) { };
+
+function getMark(isPassed) {
+  return isPassed ? '✅' : '❌';
+}
+
+const testArray = function (name, func, expected, ...args) {
+  const actual = func(...args);
+  const isPassed = expected.join() === actual.join();
+
+  return [name, ...args, actual, expected, getMark(isPassed)];
+}
+
+const testAll = function () {
+  const result = [
+    ['name', 'parameter', 'expected', 'actual', 'isPassed'],
+    testArray('filterEvenNumbers', filterEvenNumbers, [2, 4], [1, 2, 3, 4, 5]),
+    testArray('filterLongWords', filterLongWords, ["banana"],
+      ["apple", "banana", "kiwi", "grape"]),
+    testArray('filterAdults', filterAdults, [{ name: "Bob", age: 35 }], [{ name: "Alice", age: 25 }, { name: "Bob", age: 35 }]),
+  ];
+
+  console.table(result);
+}
+
+testAll();
